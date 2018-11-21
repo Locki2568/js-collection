cce0e4d6d5d7376f7b57af80aa8d181bb75007f2
Locki Lo <locki.lo@cargosmart.com>
2018-11-21 16:00:46 +0800

    [Locki] feat: filter group_count loop implement

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,8 +1,33 @@
 'use strict';
 
-function grouping_count(collection) {
+// function filter(collection, fn){
+//   for(let index=0; index < collection.length; index){
+    
+//   }
+// }
 
+function grouping_count(collection) {
   //implement here
+  var result = {};
+  for(let i =0; i< collection.length; i++){
+    if (result[collection[i]] == null){
+      result[collection[i]] = 1;
+      console.log("debug: "+result[collection[i]])
+    }else{
+      result[collection[i]] = result[collection[i]]+ 1;
+    }
+  }
+  return result
 }
+
+// function countNum(collection, element){
+//   var count = 0;
+//   for(let i=0; i<collection.length; i++){
+//     if(collection[i] === element){
+//       count +=1;
+//     }
+//   }
+//   return count;
+// }
 
 module.exports = grouping_count;
