c57aaa3b9ac24732cade89602f13f637652def08
Locki Lo <locki.lo@cargosmart.com>
2018-11-21 15:35:10 +0800

    [Locki] feat: filter choose_multiple_of_three filter implement

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -1,8 +1,27 @@
 'use strict';
 
 function choose_multiples_of_three(collection) {
-
   //implement here
+  return filter(collection, isMultipleOfThree)
+}
+
+function filter(collection, fn){
+  var result = []
+  for(var i=0; i<collection.length; i++){
+    if (fn(collection[i])){
+      result.push(collection[i])
+    }
+  }
+  return result
+}
+
+function isMultipleOfThree(element){
+  if (element % 3 === 0)
+  {
+    return true;
+  }else{
+    return false;
+  }
 }
 
 module.exports = choose_multiples_of_three;
