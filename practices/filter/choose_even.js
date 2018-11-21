fe2d37defe53b6c6c7f9c2031d7e49426bd83f87
Locki Lo <locki.lo@cargosmart.com>
2018-11-21 14:23:54 +0800

    [Locki] original choose_even extract isEven

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -2,13 +2,27 @@
 
 function choose_even(collection) {
   //implement here
+  // var result = []
+  // for(var i=0; i<collection.length; i++){
+  //   if (collection[i]%2 === 0){
+  //     result.push(collection[i])
+  //   }
+  // }
+  // return result
+
   var result = []
   for(var i=0; i<collection.length; i++){
-    if (collection[i]%2 === 0){
+    if (isEven(collection[i])){
       result.push(collection[i])
     }
-  }
+  } 
+  console.log("debug "+result)
   return result
+
+}
+
+function isEven(element){
+  return element % 2 === 0;
 }
 
 module.exports = choose_even;
