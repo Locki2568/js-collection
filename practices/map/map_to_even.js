bc4154cd4ac8d8490412f455506fcfd41fcf8d03
Locki Lo <locki.lo@cargosmart.com>
2018-11-21 14:40:37 +0800

    [Locki] feat: map_to_even added map

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,12 +1,16 @@
 'use strict';
-function map_to_even(collection){
+function map(collection, fn){
   for(let index=0; index<collection.length; index++){
-    collection[index] = times2(collection[index])
+    collection[index] = fn(collection[index])
   }
   return collection;
 }
 
 function times2(element){
   return element *2
+}
+
+function map_to_even(collection, fn){
+  return map(collection, times2)
 }
 module.exports = map_to_even;
