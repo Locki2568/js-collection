f431f691f7cc8905e791337e4d94390c92612362
Locki Lo <locki.lo@cargosmart.com>
2018-11-21 14:36:44 +0800

    [Locki] original map_to_eben exracted times2

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,9 +1,12 @@
 'use strict';
 function map_to_even(collection){
   for(let index=0; index<collection.length; index++){
-    collection[index] = collection[index]*2;
+    collection[index] = times2(collection[index])
   }
   return collection;
 }
 
+function times2(element){
+  return element *2
+}
 module.exports = map_to_even;
