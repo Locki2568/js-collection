aa371ac3b859c63f8a0555771930e60ac9966f67
Locki Lo <locki.lo@cargosmart.com>
2018-11-21 15:06:50 +0800

    [Locki] feat: filter choose_even filter implement

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -2,21 +2,18 @@
 
 function choose_even(collection) {
   //implement here
-  // var result = []
-  // for(var i=0; i<collection.length; i++){
-  //   if (collection[i]%2 === 0){
-  //     result.push(collection[i])
-  //   }
-  // }
-  // return result
+  
+  return filter(collection, isEven)
 
+}
+
+function filter(collection, fn){
   for(var i=0; i<collection.length; i++){
-    if (!isEven(collection[i])){
+    if (!fn(collection[i])){
       collection.splice(i,1)
     }
-  } 
+  }
   return collection
-
 }
 
 function isEven(element){
