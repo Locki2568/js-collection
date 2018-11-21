1a7b94bbc5abc9ac39968eb8d10d76c3a0f590e0
Locki Lo <locki.lo@cargosmart.com>
2018-11-21 14:35:38 +0800

    [Locki] original map_to_eben implementation

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,5 +1,9 @@
 'use strict';
 function map_to_even(collection){
-  return [2,4,6,8,10];
+  for(let index=0; index<collection.length; index++){
+    collection[index] = collection[index]*2;
+  }
+  return collection;
 }
+
 module.exports = map_to_even;
