c5d9291243dfaf7e1d9ef2d720bf579bc38a14e9
Locki Lo <locki.lo@cargosmart.com>
2018-11-21 14:52:26 +0800

    [Locki] feat: mul4add1 map implement

--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,10 +1,18 @@
 'use strict';
-var map_to_four_multiples_add_one = function(collection){
+function map_to_four_multiples_add_one (collection, fn){
+  return map(collection, mul4add1)
+  //return [5,9,13,17,21];
+}
+
+function map(collection, fn){
   for(let index =0; index<collection.length; index++){
-    collection[index] = collection[index] * 4 + 1
+    collection[index] = fn(collection[index])
   }
   return collection
-  //return [5,9,13,17,21];
-};
+}
+
+function mul4add1(element){
+  return element * 4 + 1
+}
 
 module.exports = map_to_four_multiples_add_one;
